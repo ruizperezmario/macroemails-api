40a49ac9eb6d5dc87f0ea7f96c51a013f42988f3
ruizperezmario <mario.ruiz@mercadolibre.com>
2018-06-24 20:00:45 -0300

    install heroku

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -14,8 +14,16 @@ public class Main {
     public static void main(String[] args) {
 
     	// Start embedded server at this port
-        port(5000);
+        port(getHerokuAssignedPort());
 
-        get("/hello", (req, res) -> "Hello World. I don't believe!!!");
+        get("/hello", (req, res) -> "Hello Heroku World. I don't believe!!!");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
